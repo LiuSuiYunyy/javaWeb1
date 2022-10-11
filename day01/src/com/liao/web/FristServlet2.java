d332e6a46b3f2b628db21390c64b12a43ac63adb
liao <liao@qq.com>
2022-10-11 20:23:17 +0800

    变更

---
+++ day01/src/com/liao/web/FristServlet2.java
@@ -0,0 +1,24 @@
+package com.liao.web;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/fristServlet2")
+public class FristServlet2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("hello1");
+    }
+}
